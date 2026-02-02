78226869343d229f447c855e83b0decaeabb2f24
marcelhrb <marcelhrb@gmail.com>
2026-02-02 17:57:36 -0300

    feat: add Zod error handler - Handle Zod validation errors returning 400 - Other errors return 500 with generic message - Enables proper validation testing

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { userRoutes } from "./routes/users";
 import Fastify from 'fastify'
 import cookie from '@fastify/cookie'
 import { mealsRoutes } from './routes/meals';
+import { ZodError } from 'zod';
 
 export const app = Fastify();
 
@@ -13,4 +14,17 @@ app.register(userRoutes, {
 })
 app.register(mealsRoutes, {
     prefix: 'meals',
+})
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.format(),
+    })
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
 })
